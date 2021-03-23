e1997626659ee5a8d26317c5b7be181a589dfd15
Nico Pergande <72522450+Nico-Pergande@users.noreply.github.com>
2021-03-23 13:29:52 +0100

    little fix

--- config.lua
+++ config.lua
@@ -1,7 +1,8 @@
 Config = {}
 Config.Locale = 'de' -- 'en' is possible too
-Config.Paystation = 
-{   {x = 447.21,  y = -987.9,  z = 30.69}, -- LSPD
+Config.Paystation = {
+    {x = 447.21,  y = -987.9,  z = 30.69}, -- LSPD
     {x = 1853.9,  y = 3688.15,  z = 34.27}, -- SSSD
     {x = -447.32,  y = 6013.39,  z = 31.72}, -- BCSO
     {x = -545.03,  y = -203.97,  z = 38.22}, -- Cityhall
+}
